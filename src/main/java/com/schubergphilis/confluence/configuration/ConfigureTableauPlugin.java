217ecf0b6296d27fdbcdb348159bbfdcf524d4a1
Roel Gerrits <git@roelgerrits.net>
2012-03-30 11:48:02 +0200

    license headers

--- src/main/java/com/schubergphilis/confluence/configuration/ConfigureTableauPlugin.java
+++ src/main/java/com/schubergphilis/confluence/configuration/ConfigureTableauPlugin.java
@@ -1,15 +1,32 @@
+/**
+ * Developer: Roel Gerrits
+ * Company: Schuberg Philis
+ * Date: 6/16/11
+ * Time: 4:58 PM
+ *
+ *      Licensed to the Apache Software Foundation (ASF) under one
+ *      or more contributor license agreements.  See the NOTICE file
+ *      distributed with this work for additional information
+ *      regarding copyright ownership.  The ASF licenses this file
+ *      to you under the Apache License, Version 2.0 (the
+ *      "License"); you may not use this file except in compliance
+ *      with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing,
+ *      software distributed under the License is distributed on an
+ *      "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *      KIND, either express or implied.  See the License for the
+ *      specific language governing permissions and limitations
+ *      under the License.
+ **/
+
 package com.schubergphilis.confluence.configuration;
 
 import com.atlassian.bandana.BandanaManager;
 import com.atlassian.confluence.core.ConfluenceActionSupport;
 
-/**
- * Created by IntelliJ IDEA.
- * User: rgerrits
- * Date: 6/16/11
- * Time: 4:58 PM
- * To change this template use File | Settings | File Templates.
- */
 public class ConfigureTableauPlugin extends ConfluenceActionSupport
 {
     private ConfigurationManager _configManager;
--- src/main/java/com/schubergphilis/confluence/plugins/SbpBaseMacro.java
+++ src/main/java/com/schubergphilis/confluence/plugins/SbpBaseMacro.java
@@ -1,3 +1,28 @@
+/**
+ * Developer: Roel Gerrits
+ * Company: Schuberg Philis
+ * Date: 3/30/11
+ * Time: 10:49 AM
+ * Description: This class includes some helper methods that can be used in all sbp macro's
+ *
+ *      Licensed to the Apache Software Foundation (ASF) under one
+ *      or more contributor license agreements.  See the NOTICE file
+ *      distributed with this work for additional information
+ *      regarding copyright ownership.  The ASF licenses this file
+ *      to you under the Apache License, Version 2.0 (the
+ *      "License"); you may not use this file except in compliance
+ *      with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing,
+ *      software distributed under the License is distributed on an
+ *      "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *      KIND, either express or implied.  See the License for the
+ *      specific language governing permissions and limitations
+ *      under the License.
+ **/
+
 package com.schubergphilis.confluence.plugins;
 
 import com.atlassian.bandana.BandanaManager;
@@ -8,19 +33,11 @@ import com.schubergphilis.confluence.configuration.ConfigurationManager;
 import com.schubergphilis.confluence.exceptions.AuthenticationException;
 import com.schubergphilis.confluence.exceptions.ValidationException;
 import org.apache.log4j.Logger;
-
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
-/**
- * Created by IntelliJ IDEA.
- * User: rgerrits
- * Date: 3/30/11
- * Time: 10:49 AM
- * This class includes some helper methods that can be used in all sbp macro's
- */
 public abstract class SbpBaseMacro extends com.atlassian.renderer.v2.macro.BaseMacro
 {
     protected WebResourceManager webResourceManager;
